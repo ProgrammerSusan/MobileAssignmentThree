fed26dcddf3aa71ee374d32f7d488ddb8a083751
ProgrammerSusan <hatems2017@gmail.com>
2020-02-25 17:52:23 -0500

    created retirement object

--- App1/app/src/main/java/com/example/myapplication/RetirementCalculator.java
+++ App1/app/src/main/java/com/example/myapplication/RetirementCalculator.java
@@ -3,9 +3,20 @@ package com.example.myapplication;
 import java.lang.Math;
 
 public class RetirementCalculator {
+    private float currentPrincipal;
+    private float annualAddition;
+    private float rate;
+    private int years;
+
+    public RetirementCalculator(float currentPrincipal, float annualAddition, float rate, int years){
+        this.currentPrincipal = currentPrincipal;
+        this.annualAddition = annualAddition;
+        this.rate = rate;
+        this.years = years;
+    }
 
     //method to calculate retirement
-    public int[] calculateRetirement(float currentPrincipal, float annualAddition, float rate,  int years){
+    public int[] calculateRetirement(){
         int [] retirement = new int [years];
         for(int i = 0; i < retirement.length; i++){
             float input = (currentPrincipal + 100 * annualAddition/rate) * (float)Math.pow(1 + rate/100, i) - 100 * (annualAddition/rate);
