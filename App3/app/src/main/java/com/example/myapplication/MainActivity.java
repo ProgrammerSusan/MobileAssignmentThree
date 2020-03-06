feffa301a4209f4db09b1582395f07444a1e8330
ProgrammerSusan <hatems2017@gmail.com>
2020-03-05 19:59:51 -0500

    interface and model tweaks

--- App3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ App3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
-    private Model model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,11 +14,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void encrypt(View v){
+        EditText message = (EditText)findViewById(R.id.message);
+        String msg = message.getText().toString();
+        Model model = new Model();
 
+        message.setText(model.encrypt(msg));
     }
 
     public void decrypt(View v){
+        EditText message = (EditText)findViewById(R.id.message);
+        String msg = message.getText().toString();
+        Model model = new Model();
 
+        message.setText(model.encrypt(msg));
     }
 
     public void keyScreen(View v){
