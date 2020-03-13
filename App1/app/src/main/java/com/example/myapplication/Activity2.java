f00f8bc1e378fa197725682d8c9261485453be82
sarahyaw <syaw@emich.edu>
2020-03-13 12:51:48 -0400

    layout changes & unversioned files added

--- App1/app/src/main/java/com/example/myapplication/Activity2.java
+++ App1/app/src/main/java/com/example/myapplication/Activity2.java
@@ -10,19 +10,23 @@ import android.widget.*;
 
 public class Activity2 extends AppCompatActivity {
     private RetirementCalculator retirement = MainActivity.retirement;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final int dp = (int)(getResources().getDisplayMetrics().density);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
         TableLayout growth =(TableLayout)findViewById(R.id.growth);
         int [] amount = retirement.calculateRetirement();
         for(int i = 1; i <= amount.length; i++){
             TableRow tr1 = new TableRow(this);
-            tr1.setLayoutParams(new ActionBar.LayoutParams( ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.WRAP_CONTENT));
+            tr1.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.WRAP_CONTENT));
             TextView year = new TextView(this);
-            year.setText(i+"");
+            year.setText(i+"  ");
             TextView payment = new TextView(this);
             payment.setText(amount[i-1]+"");
+            year.setTextSize(10*dp);
+            payment.setTextSize(10*dp);
             tr1.addView(year);
             tr1.addView(payment);
             growth.addView(tr1);
