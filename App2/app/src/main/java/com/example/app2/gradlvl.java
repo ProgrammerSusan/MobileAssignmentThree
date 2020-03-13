11dc98b7dbfb6ec3185147fd03a7c2733306f093
sarahyaw <syaw@emich.edu>
2020-03-13 13:17:22 -0400

    the Very Base File Structure

---
+++ App2/app/src/main/java/com/example/app2/gradlvl.java
@@ -0,0 +1,14 @@
+package com.example.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gradlvl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gradlvl);
+    }
+}
